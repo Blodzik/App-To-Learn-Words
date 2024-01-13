3274f062a308f2b8dafb0945cd5e6b58acb36df7
Blodzik <147334590+Blodzik@users.noreply.github.com>
2024-01-13 11:41:02 +0100

    Add files via upload

---
+++ Advance Quiz App/main.js
@@ -0,0 +1,160 @@
+const translationWord = document.getElementById('translation-input');
+const word = document.getElementById('word-input');
+const button = document.getElementById('btn');
+const scoreEl = document.getElementById('score');
+const startButton = document.getElementById('start');
+const container = document.querySelector('.container');
+const showContainer = document.querySelector('.show-container');
+const startGameCont = document.querySelector('.start-game-cont');
+const scoreContainer = document.querySelector('.score-cont');
+const gameCont = document.querySelector('.game-cont');
+
+let score = 0;
+let currentWordIndex = 0;
+
+const words = [];
+
+function addWord () {
+    if (translationWord.value.length === 0 || word.value.length === 0) {
+        alert('You need to enter translation and word first');
+    } else {
+        const currentTranslation = translationWord.value;
+        const currentWord = word.value;
+        
+        words.push({ translation: currentTranslation, word: currentWord});
+
+        console.log(words);
+
+        const show = document.createElement('p');
+        show.classList.add('show');
+        show.innerHTML = `${currentTranslation}: &nbsp; ${currentWord}`;
+        showContainer.appendChild(show);
+
+        word.value = '';
+        translationWord.value = '';
+    }
+};
+
+function startGame () {
+    if (words.length === 0) {
+        alert('You need to enter translation and word first');
+    } else {
+        startGameCont.innerHTML = '';
+
+        scoreContainer.style.display = 'block';
+        scoreEl.textContent = 'Score: ';
+
+        const wordsCont = document.createElement('div');    
+        const inputAnswer = document.createElement('input');
+        const checkButton = document.createElement('button');
+        const divAnswer = document.createElement('div');
+
+        checkButton.textContent = 'Check';
+        inputAnswer.setAttribute('placeholder', 'Type your answer here');  
+        wordsCont.classList.add('words-cont');
+        
+        wordsCont.appendChild(divAnswer);
+        wordsCont.appendChild(inputAnswer);
+        wordsCont.appendChild(checkButton);
+
+        gameCont.appendChild(scoreContainer);
+        startGameCont.appendChild(wordsCont);
+
+        divAnswer.innerHTML = words[currentWordIndex].translation;
+        
+        checkButton.addEventListener('click', () => {
+            checkAnswer(currentWordIndex, inputAnswer.value);
+            scoreContainer.style.display = 'block';
+            scoreEl.innerHTML = `Score: ${score}`;
+            gameCont.appendChild(scoreContainer);
+            
+            currentWordIndex++;  
+
+            if (currentWordIndex < words.length) {
+                divAnswer.innerHTML = words[currentWordIndex].translation;
+                inputAnswer.value = '';
+            } else {
+                startGameCont.innerHTML = 'Game Over';
+            }
+        })
+        
+        inputAnswer.addEventListener('keydown', (event) => {
+            if (event.key === 'Enter') {
+                checkAnswer(currentWordIndex, inputAnswer.value);
+                scoreContainer.style.display = 'block';
+                scoreEl.innerHTML = `Score: ${score}`;
+                gameCont.appendChild(scoreContainer);
+            
+                currentWordIndex++;  
+
+                if (currentWordIndex < words.length) {
+                    divAnswer.innerHTML = words[currentWordIndex].translation;
+                    inputAnswer.value = '';
+                } else {
+                    startGameCont.innerHTML = 'Game Over';
+                }
+            }
+        });
+        container.innerHTML = '';
+    }
+};
+
+function checkAnswer (index, userAnswer) {
+    const correctAnswer = words[index].word.toLowerCase();
+    if (userAnswer.toLowerCase() === correctAnswer) {
+        score++;
+    } else {
+        return;
+    }
+};
+
+function moveFocusToNextInput (nextInput) {
+    nextInput.focus();
+};
+
+ function displayWord (index) {
+    const divAnswer = document.createElement('div');
+    const wordsCont = document.createElement('div');   
+    divAnswer.innerHTML = words[index].translation;
+    wordsCont.appendChild(divAnswer);
+    startGameCont.appendChild(wordsCont);
+} 
+
+function displayWord (index) {
+    const wordsCont = document.createElement('div');    
+    const inputAnswer = document.createElement('input');
+    const checkButton = document.createElement('button');
+    const divAnswer = document.createElement('div');
+
+    checkButton.textContent = 'Check';
+    inputAnswer.setAttribute('placeholder', 'Type your answer here');  
+    wordsCont.classList.add('words-cont');
+    divAnswer.innerHTML = words[index].translation;
+    
+    wordsCont.appendChild(divAnswer);
+    wordsCont.appendChild(inputAnswer);
+    wordsCont.appendChild(checkButton);
+    startGameCont.appendChild(wordsCont);
+}
+
+translationWord.addEventListener('keydown', (event) => {
+    if(event.key === 'Enter') {
+        event.preventDefault();
+        moveFocusToNextInput(word);
+    }
+})
+
+word.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        addWord();
+        moveFocusToNextInput(translationWord);
+    }
+})
+
+startButton.addEventListener('click', startGame);
+
+button.addEventListener('click', addWord);
+
+
+
+
